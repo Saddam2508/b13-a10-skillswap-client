72e0be88d59e562cac8cd56275545cacd1fbcc34
Md Saddam Hossain <eyarinsultana@gmail.com>
2026-06-30 06:58:13 +0600

    payment page updated

--- src/lib/actions/payment.js
+++ src/lib/actions/payment.js
@@ -5,5 +5,5 @@ import { getTransactions } from "../api/payment";
 export async function getTransactionsAction() {
   const result = await getTransactions();
 
-  return result.data;
+  return result?.data || [];
 }
--- src/lib/api/payment.js
+++ src/lib/api/payment.js
@@ -1,4 +1,4 @@
 import { serverFetch } from "../core/server";
 
 export const getTransactions = () =>
-  serverFetch("/api/admin/transactions");
+  serverFetch("/api/admin/payments");
